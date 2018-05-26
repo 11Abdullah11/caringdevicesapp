0138af43656753b69b4d2ccce2d814290fd54970
Abdullah Zafar <abdullahzafar4876@yahoo.com>
2018-05-26 12:32:11 +0500

    temperature chart

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -65,23 +65,62 @@ $(document).ready(function () {
             labelString: 'ECG(mV)',
             display: true
           },
-     elements: {
-       line: {
-          tension: 0, // disables bezier curves
-       }
-      }
-    }]
+	 }]
   }
 
-  //Get the context of the canvas element we want to select
+ 
+  var tempData = {
+    labels: timeData,
+    datasets: [
+       {
+        fill: false,
+        label: 'temperature',
+        yAxisID: 'temperature',
+		lineTension: 0,
+        borderColor: "rgba(24, 120, 240, 1)",
+        pointBoarderColor: "rgba(24, 120, 240, 1)",
+        backgroundColor: "rgba(24, 120, 240, 0.4)",
+        pointHoverBackgroundColor: "rgba(24, 120, 240, 1)",
+        pointHoverBorderColor: "rgba(24, 120, 240, 1)",
+        data: ecgData
+      }
+    ]
+  }
+
+  var tempOption = {
+    title: {
+      display: true,
+      text: 'Temperature ',
+      fontSize: 36
+    },
+    scales: {
+     yAxes: [{
+          id: 'temperature',
+          type: 'linear',
+          scaleLabel: {
+            labelString: 'T(C)',
+            display: true
+          }
+	 }]
+  }
+
+  
+
+	//Get the context of the canvas element we want to select
   var ctx = document.getElementById("myChart").getContext("2d");
+  var tempctx = document.getElementById("tempChart").getContext("2d");
   var optionsNoAnimation = { animation: false }
   var myLineChart = new Chart(ctx, {
     type: 'line',
     data: data,
     options: basicOption
   });
-
+  var tempChart = new Chart(tempctx,{
+	type: 'line',
+	data: tempData,
+	options: tempOption
+  
+  });
   var ws = new WebSocket('wss://' + location.host);
   ws.onopen = function () {
     console.log('1Successfully connect WebSocket');
@@ -95,13 +134,13 @@ $(document).ready(function () {
       }
       var rtime = obj.time;
       var time = timeData[timeData.length-1];
-      console.log(typeof(rtime));
-      var ecgarr = obj.ecg;
+	  tempdata.push(obj.temperature);
+	  var ecgarr = obj.ecg;
       for (var i=0;i<ecgarr.length;i++){
-	var d = new Date();
-	var n = d.getTime() - startDate.getTime();
-        ecgData.push(ecgarr[i]);
-        timeData.push(n/1000);
+		  var d = new Date();
+		  var n = d.getTime() - startDate.getTime();
+		  ecgData.push(ecgarr[i]);
+		  timeData.push(floor(n/1000));
       }
       // only keep no more than 50 points in the line chart
       const maxLen = 50;
@@ -109,8 +148,10 @@ $(document).ready(function () {
       if (len > maxLen) {
         timeData.shift();
         ecgData.shift();
-      }
+		tempData.shift();
+	  }
 
+	  tempChart.update();
       myLineChart.update();
     } catch (err) {
       console.error(err);
