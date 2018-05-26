87b48a3db1183eca1d2f491d7df8eae36a2e3943
Abdullah Zafar <abdullahzafar4876@yahoo.com>
2018-05-26 12:35:02 +0500

    temperature chart

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -66,7 +66,7 @@ $(document).ready(function () {
             display: true
           },
 	 }]
-  }
+  };
 
  
   var tempData = {
@@ -102,7 +102,7 @@ $(document).ready(function () {
             display: true
           }
 	 }]
-  }
+  };
 
   
 
