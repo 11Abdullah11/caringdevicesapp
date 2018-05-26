68f7d9f4a03b33f793132b39e8ca6b9d461ac1d8
Abdullah Zafar <abdullahzafar4876@yahoo.com>
2018-05-26 23:23:16 +0500

    charts size

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -114,7 +114,7 @@ $(document).ready(function () {
   });
   var ws = new WebSocket('wss://' + location.host);
   ws.onopen = function () {
-    console.log('1Successfully connect WebSocket');
+    console.log('Successfully connect WebSocket');
   }
   ws.onmessage = function (message) {
     console.log('receive message' + message.data);
